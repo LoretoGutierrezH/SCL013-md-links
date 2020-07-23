657944b1907f417eed21a5374d19219797145680
LoretoGutierrezH <loretocgh@gmail.com>
2020-07-23 10:44:38 -0400

    lectura de archivo .md lista + package.json con colors

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-module.exports = () => {
-  // ...
-};
+const colors = require('colors');
+const fs = require('fs');
+
+const readMD = (path) => {
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err)
+    }
+    console.log('Archivo leído'.green);
+    console.log(data.toString());
+    return data;
+  })
+}
+
+file = {};
+file.readMD = readMD;
+
+
+module.exports = file;
