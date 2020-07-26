3ba0c6128fe66fb093e3ebc46b1424b7f50504da
LoretoGutierrezH <loretocgh@gmail.com>
2020-07-26 18:09:46 -0400

    usando markdown-it y jsdom para analizar archivo md y extrar text y href de cada link, pequeño cambio a forma de exportar las funciones de index.js, todo funcionando OK

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const colors = require('colors');
 const fs = require('fs');
+const md = require('markdown-it')();
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+
+//ejemplo de uso --> let result = md.render('lo que quiero transformar en html');
+//console.log(result);
+
+const filterTextAndHref = (DOM, path) => {
+ const anchors = DOM.window.document.querySelectorAll('a');
+ const anchorsArray = Array.from(anchors);
+ const textAndHref = anchorsArray.map(a => {
+   return {
+     text: a.innerHTML,
+     href: a.href,
+     file: path
+   };
+ })
+  console.log(textAndHref);
+  //return textAndHref;
+
+}
 
 const readMD = (path) => {
   fs.readFile(path, (err, data) => {
@@ -7,13 +28,23 @@ const readMD = (path) => {
       console.log(err)
     }
     console.log('Archivo leído'.green);
-    console.log(data.toString());
-    return data;
+    let outputHtml = md.render(data.toString());
+    
+    const DOM = new JSDOM(outputHtml);
+    filterTextAndHref(DOM, path);
+
+    //console.log(data.toString());
+    //return data;
   })
 }
 
-file = {};
+/* file = {};
 file.readMD = readMD;
 
 
-module.exports = file;
+module.exports = file; */
+
+module.exports = {
+  readMD: readMD,
+}
+
--- main.js
+++ main.js
@@ -1,10 +1,29 @@
-const file = require('./index');
+const indexFunctions = require('./index');
 const arguments = process.argv;
+
+
 
 //console.log(arguments[2], arguments[3]);
 
 const filePath = arguments[2];
 
 
-file.readMD(filePath);
+indexFunctions.readMD(filePath);
+
+//PROBANDO SINTÁXIS DE PROMESAS
+/* const validateURL = (url) => {
+  return new Promise((resolve, reject) => {
+    setTimeout((actualURL) => {
+      resolve(`URL ${actualURL} resuelta correctamente.`);
+    }, 1500, url)
+  }).then(data => {
+    console.log(`URL ${actualURL} resuelta correctamente`);
+    console.log(data);
+  })
+  .catch(error => {
+    console.log(error);
+  })
+}
+
+console.log(validateURL('www.google.com')); */
 
