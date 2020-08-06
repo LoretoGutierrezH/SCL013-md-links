d2ac502f910a6e9f487dd993f0aa826ee1a9e4c0
GenesisAstete <genesis.astete.a@gmail.com>
2020-08-05 20:20:07 -0400

    tabla promesas

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
 const indexFunctions = require('./cli');
 const [, , ...arguments] = process.argv;
 
-//console.log(arguments[2], arguments[3]);
-
 const filePath = process.argv[2];
-
 
 //después esto hay que transformarlo en mdLinks(path, options) para versión API;
 indexFunctions.mdLinks(filePath, arguments);
-
-
-
-
